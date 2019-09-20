ccb23f2ff8a8a56e77b6441ba1d2af7965c078fc
lvsportsclinic <paul@lvsportsclinic.com>
2019-09-20 01:33:34 -0700

    edit posts.js

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,5 +1,5 @@
 var posts_array = [
-    ["09/19/2019", null, "@pathto(blog/post01)", "Review"],
-    ["09/19/2019", null, "@pathto(blog/post02)", "Post 2"],
-    ["09/19/2019", null, "@pathto(blog/post03)", "Post 3"],
+    ["09/19/2019", " ", "@pathto(blog/post01)", "Review"],
+    ["09/19/2019", " ", "@pathto(blog/post02)", "Post 2"],
+    ["09/19/2019", " ", "@pathto(blog/post03)", "Post 3"],
     ];
