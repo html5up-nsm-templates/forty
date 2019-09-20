a9291c90c4d78ba039aba8ad45c14d9abe6b0069
lvsportsclinic <paul@lvsportsclinic.com>
2019-09-20 01:21:37 -0700

    posts updated

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,5 +1,5 @@
 var posts_array = [
-    ["@pathto(blog/post01)", "Discussion of Mayo Clinic Proceedings article 'Clinicians’ Guide to Cannabidiol and Hemp Oils'"],
+    ["09/19/2019", "post 1", "@pathto(blog/post01)", "Post 1"],
     ["09/19/2019", "post 2", "@pathto(blog/post02)", "Post 2"],
     ["09/19/2019", "post 3", "@pathto(blog/post03)", "Post 3"],
     ];
