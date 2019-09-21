a2ee0c290d1b62ba9bd9d27f6d5266a22c2352e4
lvsportsclinic <paul@lvsportsclinic.com>
2019-09-20 22:37:09 -0700

    update post01 in posts.js

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,6 +1,5 @@
 var posts_array = [
-    ["09/19/2019", " ", "@pathto(blog/post01)", "Article Discussion:  Clinicians’ Guide to Cannabidiol and
-Hemp Oils"],
+    ["09/19/2019", " ", "@pathto(blog/post01)", "Article Discussion:  Clinicians’ Guide to Cannabidiol and Hemp Oils"],
     ["09/19/2019", " ", "@pathto(blog/post02)", "Post 2"],
     ["09/19/2019", " ", "@pathto(blog/post03)", "Post 3"],
     ];
